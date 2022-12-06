3d62970bdeef2c9ebf25ae497661fcf10a64c791
Kishore R <92428116+kishore2202@users.noreply.github.com>
2022-12-06 09:38:33 +0530

    Create IteratingUsingForEach.js

---
+++ IteratingUsingForEach.js
@@ -0,0 +1,12 @@
+var numbers=[1,2,3,4,5,6];
+//approach 1
+numbers.forEach(function(nums) {
+    console.log(nums*2);
+});
+
+//approach 2
+function double(n)
+{
+    console.log(n);
+}
+numbers.forEach(double);
