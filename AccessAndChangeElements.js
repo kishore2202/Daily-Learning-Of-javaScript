3cf7613dba3eff9673166ff2466eff72beb94428
Kishore R <92428116+kishore2202@users.noreply.github.com>
2023-01-03 12:44:43 +0530

    Create AccessAndChangeElements.js

---
+++ AccessAndChangeElements.js
@@ -0,0 +1,8 @@
+//to see the title
+document.querySelector(".main-title").innerHTML
+
+//to see the tag which holds the title
+document.querySelector(".main-title").outerHTML
+
+//to change the title
+document.querySelector(".main-title").innerHTML="Title changed"
